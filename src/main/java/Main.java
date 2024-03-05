f65ea215284990c893cfabac7bdf3d450621c56f
sharpie1330 <amberaplin14@gmail.com>
2024-03-05 23:49:37 +0900

    [feature3] 게임 진행을 위한 Main 클래스 생성 및 게임 시작, 종료 기능 구현

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import chess.Board;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final String START = "start";
+        final String END = "end";
+
+        while (true) {
+            System.out.print("체스 게임에 오신 것을 환영합니다. 시작하려면 start를 입력해주세요...");
+            String input = scanner.nextLine().toLowerCase();
+
+            if (input.equals(START)) {
+                System.out.println("체스 게임을 시작합니다.");
+                gameStart();
+            } else if (input.equals(END)) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else {
+                System.out.println("다시 입력해 주세요.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void gameStart() {
+        Board board = new Board();
+        board.initialize();
+        board.print();
+    }
+}
